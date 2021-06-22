7d22869c45179fe55486a6250d234c111948fc88
sungbeckyong <sung1683@naver.com>
2021-06-22 10:15:03 +0900

    a

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
-
-
 var http = require('http');
 http.createServer(function(request,response){
     response.writeHead(200,{'Content-type':'text/html'});
     response.end('<h1>hello</h1>');
-}).listen(8001);
-
+}).listen(80);
