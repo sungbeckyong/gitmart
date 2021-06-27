263f6a2eda345b0f0d2a204128f29fd4de039cc4
sungbeckyong <sung1683@naver.com>
2021-06-27 22:06:52 +0900

    aa

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http = require('http');
 http.createServer(function(request,response){
     response.writeHead(200,{'Content-type':'text/html'});
-    response.end('<h1>hello</h1>');
+    response.end('<h1>hellooo</h1>');
 }).listen(443);
